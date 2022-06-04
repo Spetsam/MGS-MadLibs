e015c15bc5ad03f5d8e43ee78060c3f703c5c083
Spetsam <101822684+Spetsam@users.noreply.github.com>
2022-06-04 17:17:31 +0200

    Add files via upload

---
+++ Mad Libs/madlib.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+thisf= input("Form: ")
+
+madlib = f"GitHub is such a {adj} application because I can present my code in {thisf} form!\
+    String manipulation is also interesting."
+
+print(madlib)
